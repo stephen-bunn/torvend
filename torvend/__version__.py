86e59622177e4c22c9546dcf2435eb9f51c3694b
Stephen Bunn <stephen@bunn.io>
2017-11-08 23:23:16 -0500

    Updating project structure basics

---
+++ torvend/__version__.py
@@ -0,0 +1,19 @@
+#
+# ▄▄▄▄▄      ▄▄▄   ▌ ▐·▄▄▄ . ▐ ▄ ·▄▄▄▄
+# •██  ▪     ▀▄ █·▪█·█▌▀▄.▀·•█▌▐███▪ ██
+#  ▐█.▪ ▄█▀▄ ▐▀▀▄ ▐█▐█•▐▀▀▪▄▐█▐▐▌▐█· ▐█▌
+#  ▐█▌·▐█▌.▐▌▐█•█▌ ███ ▐█▄▄▌██▐█▌██. ██
+#  ▀▀▀  ▀█▄▀▪.▀  ▀. ▀   ▀▀▀ ▀▀ █▪▀▀▀▀▀•
+#
+# Copyright (c) 2017 Stephen Bunn (stephen@bunn.io)
+# MIT License <https://opensource.org/licenses/MIT>
+
+__name__ = 'torvend'
+__description__ = 'A framework for public torrent vendor scrapers'
+__version__ = '0.0.0'
+__author__ = 'Stephen Bunn'
+__contact__ = 'stephen@bunn.io'
+__license__ = 'MIT License'
+__contributors__ = (
+    'Stephen Bunn <stephen@bunn.io>',
+)
