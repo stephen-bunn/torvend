1a1b9d23766d5574f206276d35688294ac5a48f0
Stephen Bunn <stephen@bunn.io>
2017-12-13 00:04:57 -0500

    Fixing documentation
    - Flask-Sphinx-Themes update to remove get_path()

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,7 +102,6 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme_path = [flask_sphinx_themes.get_path()]
 html_theme = 'flask'
 
 # Theme options are theme-specific and customize the look and feel of a theme
