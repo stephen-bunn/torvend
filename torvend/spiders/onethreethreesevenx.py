b71c010a7c146479a8880b1481cf859fa9a7ba96
Stephen Bunn <stephen@bunn.io>
2017-12-14 13:34:58 -0500

    Removing useless environment headers in Python files

--- torvend/spiders/onethreethreesevenx.py
+++ torvend/spiders/onethreethreesevenx.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-#
 # Copyright (c) 2017 Stephen Bunn (stephen@bunn.io)
 # GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.txt>
 
--- torvend/spiders/skytorrents.py
+++ torvend/spiders/skytorrents.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-#
 # Copyright (c) 2017 Stephen Bunn (stephen@bunn.io)
 # MIT License <https://opensource.org/licenses/MIT>
 
